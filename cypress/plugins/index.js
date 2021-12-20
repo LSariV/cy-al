0c79f26f1548288fefd342669da55dab7d050786
lakshmi-apfm <valasapalli@hotmail.com>
2021-12-20 15:59:02 -0800

    ng perf and api testing

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -38,7 +39,25 @@ module.exports = (on, config) => {
       await afterRunHook();
     });
   };
-
 }
+
+  //Cypress lighthouse code from the npm site
+  
+  const { lighthouse, pa11y, prepareAudit } = require("cypress-audit");
+ 
+
+module.exports = (on, config) => {
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    prepareAudit(launchOptions);
+  });
+
+  on("task", {
+    lighthouse: lighthouse(), // calling the function is important
+    pa11y: pa11y(), // calling the function is important
+  });
+};
+  
+
+
 
 //cypress\integration\Alz.net\Specs\blogTest.spec.js
